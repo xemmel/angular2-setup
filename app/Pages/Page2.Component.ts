24ce7c47b943ae5b423b964bfe128569a2d35352
xemmel <lacour@gmail.com>
2016-12-16 09:11:32 +0100

    Added ng-bootstrap and DatePicker example

--- app/Pages/Page2.Component.ts
+++ app/Pages/Page2.Component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
-
+import {NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     template: `
+   
         <div>
-            Page2
+           <ngb-datepicker [(ngModel)]="my" (navigate)="date = $event"></ngb-datepicker>
+            <pre>{{my | json}}</pre>
+            <pre>{{date | json}}</pre>
+
         </div>
     ` 
 })
 export class Page2Component {
-    
-}
+    my : NgbDateStruct;
+    date : Date;
+   
+};
--- app/app.Module.ts
+++ app/app.Module.ts
@@ -1,5 +1,8 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 import { AppComponent }   from './appComponent';
 
 import { AppRoutingModule }     from './app.Routing';
@@ -9,7 +12,7 @@ import { Page2Component } from './pages/Page2.Component';
 
 
 @NgModule({
-  imports:      [ BrowserModule, AppRoutingModule ],
+  imports:      [ BrowserModule, AppRoutingModule, FormsModule, NgbModule.forRoot() ],
   declarations: [ AppComponent, Page1Component, Page2Component ],
   bootstrap:    [ AppComponent ]
 })
